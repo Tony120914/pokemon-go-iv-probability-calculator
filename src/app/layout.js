0a8bd9350c99a9db396372c47c74ca7f9bf5a563
Tony Lin <ttony.lin@mail.utoronto.ca>
2024-01-26 17:08:11 -0500

    moved analytics to head

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-bs-theme='dark'>
+        <head>
+            {/* Google tag (gtag.js) */}
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-QJCFZQZD6Z"/>
+            <script dangerouslySetInnerHTML=
+                {{__html:
+                    `window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-QJCFZQZD6Z');`
+                }}
+            />
+        </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 'use client';
 import { useState, useEffect } from 'react';
-import Script from 'next/script';
 import Navbar from './components/Navbar';
 import PresetIVsButton from './components/PresetIVsButton'
 import IVsSelector from './components/IVsSelector'
@@ -62,16 +61,6 @@ export default function Home() {
     
     return (
         <>
-            {/* Google tag (gtag.js) */}
-            <Script async src="https://www.googletagmanager.com/gtag/js?id=G-QJCFZQZD6Z"/>
-            <Script id='googleAnalytics'> {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-
-                gtag('config', 'G-QJCFZQZD6Z');
-            `}</Script>
-
             <Navbar />
             <main>
                 <div>
