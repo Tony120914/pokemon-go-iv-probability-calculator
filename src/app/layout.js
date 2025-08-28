4470cede7b09c3fbaf6dc2d957797a62ccbbfc4d
Tony Lin <tonylin120914@gmail.com>
2025-08-28 15:00:18 -0400

    retired next.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,6 @@
-import { Inter } from 'next/font/google'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 // import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
     title: 'Pokemon GO IV Probability Calculator',
