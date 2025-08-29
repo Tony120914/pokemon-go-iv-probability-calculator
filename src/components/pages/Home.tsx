c2a562d23c295e9e89ef0dc096582a2170c96d91
Tony Lin <tonylin120914@gmail.com>
2025-08-29 19:22:57 -0400

    search parameters

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -3,9 +3,8 @@ import Navbar from "../Navbar.js";
 import Calculator from "./home/Calculator.js";
 
 export default function Home() {
-
-    // i don't know why i have to do this for bootstrap tooltips to work
     useEffect(()=>{
+        // i don't know why i have to do this for bootstrap tooltips to work
         (async () => {
             const bootstrap = await import("bootstrap");
             const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
